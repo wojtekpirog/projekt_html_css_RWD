14a1ec1d3833e01ec43e4b14a9f437ea084fe32e
Wojciech Piróg <wojtekpirog12@interia.pl>
2025-04-11 00:56:33 +0200

    Reset project structure

--- script.js
+++
@@ -1,6 +0,0 @@
-let toggler = document.getElementsByClassName("navbar-toggler")[0];
-let navbarLinks = document.getElementsByClassName("navbar-links")[0];
-
-toggler.addEventListener("click", () => {
-  navbarLinks.classList.toggle("collapse");
-})
