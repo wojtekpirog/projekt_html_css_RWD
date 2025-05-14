5c3b6138dfe7b6d31ab946cd331eda698cf7f0ac
Wojciech Piróg <wojtekpirog12@interia.pl>
2025-05-14 23:48:21 +0200

    Add section with achievments

---
+++ js/slick.js
@@ -0,0 +1,23 @@
+$(".team__slider").slick({
+  arrows: false,
+  speed: 800,
+  // autoplay: true,
+  mobileFirst: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  pauseOnHover: true,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 2
+      }
+    },
+    {
+      breakpoint: 992,
+      settings: {
+        slidesToShow: 3
+      }
+    }
+  ]
+});
